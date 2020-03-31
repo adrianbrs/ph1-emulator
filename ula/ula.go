15e2eacbfabedcd4d0ea2abf82f7b63c19007edf
Luiz Gallas <luizgallas@outlook.com>
2020-03-30 19:25:51 -0500

    Alterado tipo do memoryValue

--- ula/ula.go
+++ ula/ula.go
@@ -1,27 +1,29 @@
 package ula
 
 //ExecuteOperation executa a operação de acordo com o opcode
-func ExecuteOperation(ulaOp string, memoryValue int, *ac int) {
+func ExecuteOperation(ulaOp string, memoryValue int, ac int) int {
 	acInt8 := int8(ac)
+	memoryValueInt8 := int8(memoryValue)
 	switch ulaOp {
 	case "NOT":
 		ac = ^ac
 	case "ADD":
-		ac += memoryValue
+		ac += memoryValueInt8
 	case "SUM":
-		ac -= memoryValue
+		ac -= memoryValueInt8
 	case "MUL":
-		ac *= memoryValue
+		ac *= memoryValueInt8
 	case "DIV":
-		ac /= memoryValue
+		ac /= memoryValueInt8
 	case "AND":
-		acInt8 = acInt8 & memoryValue
+		acInt8 = acInt8 & memoryValueInt8
 		ac = int(acInt8)
 	case "OR":
-		acInt8 = acInt8 | memoryValue
+		acInt8 = acInt8 | memoryValueInt8
 		ac = int(acInt8)
 	case "XOR":
-		acInt8 = acInt8 ^ memoryValue
+		acInt8 = acInt8 ^ memoryValueInt8
 		ac = int(acInt8)
 	}
+	return ac
 }
