92619d941a5d7742d072ca3899d2a4a4cbed8cde
Luiz Gallas <luizgallas@outlook.com>
2020-04-08 11:18:22 -0300

    Adicionados comentários

--- control/operations.go
+++ control/operations.go
@@ -5,38 +5,46 @@ import (
 	"ph1-emulator/regs"
 )
 
+//ExecuteOpNOP dont execute nothing
 func (uc *unityControl) ExecuteOpNOP() {}
 
+//ExecuteOpLDR execute a load operation
 func (uc *unityControl) ExecuteOpLDR(value int) {
 	regs.RegAC.Value = memory.VirtualMemory.GetValue(value)
 }
 
+//ExecuteOpSTR execute a store operation
 func (uc *unityControl) ExecuteOpSTR(value int) {
 	memory.VirtualMemory.SetValue(value, regs.RegAC.Value)
 }
 
+//ExecuteOpJMP execute a jump operation
 func (uc *unityControl) ExecuteOpJMP(value int) {
 	regs.RegPC.SetValue(value)
 }
 
+//ExecuteOpJEQ execute a conditional equals operation
 func (uc *unityControl) ExecuteOpJEQ(value int) {
 	if regs.RegAC.Value == 0 {
 		regs.RegPC.SetValue(value)
 	}
 }
 
+//ExecuteOpJG execute a conditional bigger operation
 func (uc *unityControl) ExecuteOpJG(value int) {
 	if regs.RegAC.Value > 0 {
 		regs.RegPC.SetValue(value)
 	}
 }
 
+//ExecuteOpJL execute a conditional lower operation
 func (uc *unityControl) ExecuteOpJL(value int) {
 	if regs.RegAC.Value < 0 {
 		regs.RegPC.SetValue(value)
 	}
 }
 
+//ExecuteOpHLT execute a halt operation
 func (uc *unityControl) ExecuteOpHLT() {
 	uc.Houte = true
 }
--- ula/ula.go
+++ ula/ula.go
@@ -5,44 +5,54 @@ import (
 	"ph1-emulator/regs"
 )
 
+//ULA struct
 type ULA struct{}
 
 var ula = &ULA{}
 
+//GetInstance create a ULA instance
 func GetInstance() *ULA {
 	return ula
 }
 
+//ExecuteOpNOT execute bitwise NOT operation
 func (ula *ULA) ExecuteOpNOT() {
 	regs.RegAC.Value = int(^int8(regs.RegAC.Value))
 }
 
+//ExecuteOpADD execute sum operation
 func (ula *ULA) ExecuteOpADD(addr int) {
 	regs.RegAC.Value += memory.VirtualMemory.GetValue(addr)
 }
 
+//ExecuteOpSUB execute subtraction operation
 func (ula *ULA) ExecuteOpSUB(addr int) {
 	regs.RegAC.Value -= memory.VirtualMemory.GetValue(addr)
 }
 
+//ExecuteOpMUL execute multiplication operation
 func (ula *ULA) ExecuteOpMUL(addr int) {
 	regs.RegAC.Value *= memory.VirtualMemory.GetValue(addr)
 }
 
+//ExecuteOpDIV execute division operation
 func (ula *ULA) ExecuteOpDIV(addr int) {
 	regs.RegAC.Value /= memory.VirtualMemory.GetValue(addr)
 }
 
+//ExecuteOpAND execute bitwise AND operation
 func (ula *ULA) ExecuteOpAND(addr int) {
 	value := memory.VirtualMemory.GetValue(addr)
 	regs.RegAC.Value = int(int8(regs.RegAC.Value) & int8(value))
 }
 
+//ExecuteOpOR execute bitwise OR operation
 func (ula *ULA) ExecuteOpOR(addr int) {
 	value := memory.VirtualMemory.GetValue(addr)
 	regs.RegAC.Value = int(int8(regs.RegAC.Value) | int8(value))
 }
 
+//ExecuteOpXOR execute bitwise XOR operation
 func (ula *ULA) ExecuteOpXOR(addr int) {
 	value := memory.VirtualMemory.GetValue(addr)
 	regs.RegAC.Value = int(int8(regs.RegAC.Value) ^ int8(value))
