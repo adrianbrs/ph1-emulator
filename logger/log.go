7db4a475ee7bd5f6036b73f78232def4350a1a8b
Adrian Cerbaro <adriancerbaro@gmail.com>
2020-04-10 03:33:23 -0300

    'N Instructions Executed' adicionado ao log final

--- logger/log.go
+++ logger/log.go
@@ -3,17 +3,22 @@ package logger
 import (
 	"fmt"
 	"ph1-emulator/memory"
+	"ph1-emulator/numbers"
 	"ph1-emulator/regs"
 )
 
 // LogFinalState exibe os valores atuais dos registradores,
 // e os valores atuais dos endereços modificados na memória
-func LogFinalState() {
+func LogFinalState(executionCount int) {
+	// Log do número de execuções
+	fmt.Println()
+	fmt.Printf("%d instructions executed\n", executionCount)
+
 	// Log dos registradores
 	fmt.Println()
 	fmt.Println("Registers:")
-	fmt.Printf("AC %02X\n", regs.RegAC.Value)
-	fmt.Printf("PC %02X\n", regs.RegPC.GetValue())
+	fmt.Printf("AC %s\n", numbers.IntToHex(regs.RegAC.GetValue()))
+	fmt.Printf("PC %s\n", numbers.IntToHex(regs.RegPC.GetValue()))
 	fmt.Println()
 
 	// Log da memória
@@ -21,7 +26,7 @@ func LogFinalState() {
 
 	// Pega todos os endereços que foram modificados após a memória ser carregada
 	// e exibe os valores atuais correspondentes
-	for _, addr := range memory.VirtualMemory.GetChangedAddresses() {
-		fmt.Printf("%02X %02X\n", addr, memory.VirtualMemory.GetValue(addr))
+	for addr, val := range memory.VirtualMemory.GetLastChanges() {
+		fmt.Printf("%s %s\n", numbers.IntToHex(addr), numbers.IntToHex(val))
 	}
 }
