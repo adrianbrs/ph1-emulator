3bc6f12cf8ce21ac389ca1be244b3faa9713be2b
Adrian Cerbaro <adriancerbaro@gmail.com>
2020-04-10 03:36:30 -0300

    Correçoes na verificaçao de tamanho de palavra e endereços

--- memory/virtualMemory.go
+++ memory/virtualMemory.go
@@ -2,8 +2,7 @@ package memory
 
 import (
 	"log"
-	"ph1-emulator/config"
-	"strconv"
+	"ph1-emulator/numbers"
 )
 
 // virtualMemory representação virtual de uma memória
@@ -30,6 +29,12 @@ func (mem *virtualMemory) GetValue(addr int) int {
 	return mem.Data[parseAddr(addr)]
 }
 
+// GetWordValue retorna o valor presente no endereço especificado como um
+// inteiro convertido para int8 e depois int
+func (mem *virtualMemory) GetWordValue(addr int) int {
+	return int(int8(mem.GetValue(addr)))
+}
+
 // SetLoaded define que a memória virtual foi carregada com sucesso
 func (mem *virtualMemory) SetLoaded() {
 	mem.loaded = true
@@ -42,8 +47,12 @@ func (mem *virtualMemory) IsLoaded() bool {
 
 // GetChangedAddresses retorna uma lista com os endereços de memória
 // que forma modificados
-func (mem *virtualMemory) GetChangedAddresses() []int {
-	return mem.runtimeChanges
+func (mem *virtualMemory) GetLastChanges() map[int]int {
+	changes := make(map[int]int, len(mem.runtimeChanges))
+	for _, addr := range mem.runtimeChanges {
+		changes[addr] = mem.GetWordValue(addr)
+	}
+	return changes
 }
 
 // VirtualMemory => Memória Virtual
@@ -66,17 +75,18 @@ func newMemory() (mem *virtualMemory) {
 }
 
 func parseAddr(addr int) int {
-	res, err := strconv.ParseUint(strconv.Itoa(addr), 10, config.AddrLength)
+	res, err := numbers.ValidateAddress(addr)
 	if err != nil {
 		log.Fatalf("virtualMemory address overflow: %d", addr)
 	}
-	return int(res)
+	return res
 }
 
 func parseWord(val int) int {
-	res, err := strconv.ParseUint(strconv.Itoa(val), 10, config.WordLength)
+	res, err := numbers.ValidateWord(val)
+
 	if err != nil {
 		log.Fatalf("virtualMemory word length overflow: %d", val)
 	}
-	return int(res)
+	return res
 }
