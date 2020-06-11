bbcca63c85c99062489f5359377ac2ea0f8df548
Adrian Cerbaro <adriancerbaro@gmail.com>
2020-06-10 21:08:32 -0300

    Update signals

--- main.go
+++ main.go
@@ -13,17 +13,28 @@ import (
 func main() {
 	// Inicia a rotina de espera para o sinal de interrupção
 	halt := make(chan bool, 1)
+	done := make(chan bool, 1)
 	SetupExitHandler(halt)
 
 	// Cria uma goroutine para ficar aguardando o sinal de parada
 	go func() {
 		// Aguarda pelo sinal de parada (HLT)
-		<-halt
+		success := <-halt
 
 		// Exibe as informações do estado final dos registradores e memória
 		logger.LogFinalState(control.UnityControl.Counter)
 
-		// Saí da execução do programa
+		// Se o valor recebido em halt for falso, quer dizer que o
+		// programa foi interrompido pelo usuário (Ctrl+C)
+		if !success {
+			// Exibe mensagem de parada
+			fmt.Print("\n\n")
+			fmt.Println("\r- Ctrl+C pressed in terminal, program exited.")
+		}
+
+		// Envia true para o canal done para que saia da função main
+		// e termine a execução do programa
+		done <- true
 		os.Exit(0)
 	}()
 
@@ -34,9 +45,9 @@ func main() {
 	control.UnityControl.Start(halt)
 
 	// Aguarda o sinal de parada para terminar a execução,
-	// assim damos tempo para a goroutine acima ser executada
-	// e exibir o log final
-	<-halt
+	// assim garantimos que a goroutine acima será executada
+	// exibindo o log final
+	<-done
 }
 
 // SetupExitHandler inicia uma goroutine que ficara esperando
@@ -56,12 +67,8 @@ func SetupExitHandler(halt chan bool) {
 		// Interrompe a execução da unidade de controle
 		control.UnityControl.Houte = true
 
-		// Exibe mensagem de parada
-		fmt.Print("\n\n")
-		fmt.Println("\r- Ctrl+C pressed in terminal, program exited.")
-
 		// Envia o sinal de halt (HLT) caso a unidade de controle ainda
 		// não tenha sido iniciada para enviar o sinal
-		halt <- true
+		halt <- false
 	}()
 }
